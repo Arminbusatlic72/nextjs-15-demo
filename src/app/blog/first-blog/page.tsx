3c213a8b657729435f1750b9a694c38158feafba
Armin <busatar@cronos.be>
2024-12-15 17:00:26 +0100

    adding page routes to application, adding nested routes, dynamic routes and creating route groupe without affecting routes

---
+++ src/app/blog/first-blog/page.tsx
@@ -0,0 +1,3 @@
+export default function FirstBlogPage() {
+  return <h1>This my first blog</h1>;
+}
---
+++ src/app/components/greet.tsx
@@ -0,0 +1,4 @@
+export const Greet = () => {
+  console.log("Greetings from me");
+  return <h1>Greetings from me</h1>;
+};
