541992a38b4873563486d4423bb812b29f1c882b
Armin <busatar@cronos.be>
2024-12-15 21:13:10 +0100

    fetching data from server components, handling loading message and error message

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,28 @@
+import { use } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+export default async function UserServer() {
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      <ul className="space-y-2">
+        {users.map((user: any) => (
+          <li key={user.id} className="bg-gray-100 p-2 rounded shadow">
+            <p className="text-lg font-semibold">{user.name}</p>
+            <p className="text-sm text-gray-600">{user.email}</p>
+            <p className="text-sm text-gray-600">{user.phone}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
